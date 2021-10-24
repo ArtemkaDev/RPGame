7225fac0c0689db57cdb632210eeeccd0bc48de2
ArtemkaDev <mine38883@gmail.com>
2021-10-24 20:04:43 +0200

    Artemka

--- Game/main.py
+++ Game/main.py
@@ -1,13 +1,30 @@
-from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
+from kivy.graphics import Rectangle
 from kivy.app import App
 
 
 #main class
-class Main(App):
-    def build(self):
-        return Button(text = "Lol")
+class GameWidget(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._keyboard = Window.request_keyboard(self._on_keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self._on_key_down)
+
+    def _on_keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_key_down)
+        self._keyboard = None
+
+    def _on_key_down(self, keyboard, keycode, text, modifires):
+        with self.canvas:
+            Rectangle()
 
 
 #start
+class MyApp(App):
+    def build(self):
+        return GameWidget()
+
+
 if __name__ == '__main__':
-    Main().run()
+    MyApp().run()
