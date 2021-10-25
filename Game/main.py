5561bc97babad4b18968c1b535cb00abbf37cc7b
ArtemkaDev <mine38883@gmail.com>
2021-10-25 22:43:41 +0200

    Andrij

--- Game/main.py
+++ Game/main.py
@@ -1,12 +1,17 @@
 from modules.jsons import jsons
 import pygame
+import json
+import os
 
-config_json = jsons().check()
-print(config_json)
+jsons().check()
+
+with open(f"{os.getenv('APPDATA')}\ProjectRedAdventure\config.json", "r") as json_file:
+    config_json = json.load(json_file)
 
 #stats of game
 if config_json['screen']['mode'] == 1:
     pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    pygame.display.set_caption('ProjectRed Adventures')
 elif config_json['screen']['mode'] == 2:
     pygame.display.set_mode((config_json['screen']['width'], config_json['screen']['height']))
 else:
