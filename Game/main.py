66a96a03e4fb135caf4766e07e30c97a564ddb06
ArtemkaDev <mine38883@gmail.com>
2021-10-25 21:48:17 +0200

    Artemka

--- Game/main.py
+++ Game/main.py
@@ -19,11 +19,16 @@ if os.path.exists(name):
     with open(f"{name}\config.json", "w+") as f:
         json.dump(file_json, f)
 
+with open(f"{name}\config.json", "r") as json_file:
+    config_json = json.load(json_file)
+
 #stats of game
-if type_screen == 1:
+if config_json['screen']['mode'] == 1:
     pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-elif type_screen == 2:
+elif config_json['screen']['mode'] == 2:
     pygame.display.set_mode(())
+else:
+    print("Error")
 
 #run
 while True:
