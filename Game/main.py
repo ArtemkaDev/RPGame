f7180478ffbf80b76e1a2753eb606f002f0f3143
xyzHooDlEzyx <andrijbala8@gmail.com>
2021-10-26 09:25:17 +0300

    Andrij

--- Game/main.py
+++ Game/main.py
@@ -11,7 +11,12 @@ with open(f"{os.getenv('APPDATA')}\ProjectRedAdventure\config.json", "r") as jso
 #stats of game
 if config_json['screen']['mode'] == 1:
     pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-    pygame.display.set_caption('ProjectRed Adventures')
+    pygame.display.set_caption('ProjectRed Adventures')#game name
+    #create player
+    x = 200
+    y = 200
+    img = pygame.image.load('img/Layer 1.png')
+
 elif config_json['screen']['mode'] == 2:
     pygame.display.set_mode((config_json['screen']['width'], config_json['screen']['height']))
 else:
