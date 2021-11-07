8ff3a765c4f93cf32065713f7b1fcbbfad2040e4
ArtemkaDev <mine38883@gmail.com>
2021-11-07 15:41:15 +0100

    Artemka

--- Game/main.py
+++ Game/main.py
@@ -1,62 +1,20 @@
 from modules.player import Solider
 from modules.jsons import jsons
+from launcher import launcher
 import modloader
 import pygame
 import json
 import sys
 import os
 
-pygame.init()
-
 # value
 test_mod = True
 in_game = False
 # сделать то что снизу на False когда сделаешь авторизацию
-authorizat = True
+authorizat = False
 # сверху
 moving_left = False
 moving_right = False
-base_font = pygame.font.SysFont("Futura", 48)
-
-
-#json
-jsons().check()
-
-with open(os.path.expanduser(f"{os.getenv('APPDATA')}\ProjectRedAdventure\config.json"), "r") as json_file:
-    config_json = json.load(json_file)
-
-#screen stats
-if config_json['screen']['mode'] == 1:
-    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN | pygame.DOUBLEBUF, 16)
-elif config_json['screen']['mode'] == 2:
-    screen = pygame.display.set_mode((config_json['screen']['width'], config_json['screen']['height']), pygame.RESIZABLE)
-else:
-    print("Error")
-
-
-pygame.display.set_caption('ProjectRed Adventures')
-
-#fps
-clock = pygame.time.Clock()
-fps = config_json['fps']
-
-
-#create player
-player = Solider("default", 200, 600, 3, 10, screen)
-
-
-#background
-def draw_bg():
-    screen.fill((144, 201, 120))
-    p1 = (300, 1920)
-    p2 = (0, 1920)
-    # pygame.draw.line(screen, Color("red"), p1, p2, width=3)
-
-
-#mod
-modloader.mod(test_mod).start()
-modloader.mod(test_mod).load()
-
 
 
 # stop
@@ -65,23 +23,71 @@ def stop():
     sys.exit()
 
 
+# launcher
+def launcher_start():
+    launcher()
+
+
+launcher_start()
+
+# game
+pygame.init()
+base_font = pygame.font.SysFont("Futura", 48)
+
+# json
+jsons().check()
+
+with open(os.path.expanduser(f"{os.getenv('APPDATA')}\ProjectRedAdventure\config.json"), "r") as json_file:
+    config_json = json.load(json_file)
+
+# screen stats
+if config_json['screen']['mode'] == 1:
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN | pygame.DOUBLEBUF, 16)
+elif config_json['screen']['mode'] == 2:
+    screen = pygame.display.set_mode((config_json['screen']['width'], config_json['screen']['height']),
+                                     pygame.RESIZABLE)
+else:
+    print("Error")
+
+pygame.display.set_caption('ProjectRed Adventures')
+
+# fps
+clock = pygame.time.Clock()
+fps = config_json['fps']
+
+# create player
+player = Solider("default", 200, 600, 3, 10, screen)
+
+
+# background
+def draw_bg():
+    screen.fill((144, 201, 120))
+    p1 = (300, 1920)
+    p2 = (0, 1920)
+    # pygame.draw.line(screen, Color("red"), p1, p2, width=3)
+
+
+# mod
+modloader.mod(test_mod).start()
+modloader.mod(test_mod).load()
+
 # input
 user_text = ''
 
 # run
 stop_run: bool = False
 
-
-
 if __name__ == '__main__':
+    if not authorizat:
+        stop()
     while True:
-        #draw
+        # draw
         draw_bg()
         player.update_animation()
         player.draw()
         screen.blit(base_font.render(str(int(clock.get_fps())), 0, pygame.Color("white")), (10, 10))
         pygame.display.update()  # update
-        #event
+        # event
         if player.alive:
             if moving_left or moving_right:
                 player.update_action(1)  # run
