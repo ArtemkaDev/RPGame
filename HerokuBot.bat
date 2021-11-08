9949f9b3d4725e45b14c4531cbe54018e396080e
ArtemkaDev <mine38883@gmail.com>
2021-11-08 12:22:27 +0100

    Artemka

--- HerokuBot.bat
+++
@@ -1,5 +0,0 @@
-cd C:\Users\38050\Desktop\RPGame\bot
-git add .
-git commit -am "make it better"
-git push heroku master
-pause
--- HerokuCite.bat
+++
@@ -1,5 +0,0 @@
-cd C:\Users\38050\Desktop\RPGame\cite
-git add .
-git commit -am "make it better"
-git push heroku master
-pause
